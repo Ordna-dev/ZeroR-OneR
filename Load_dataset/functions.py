9fd4049cd71ef7f3eb649f4beffe3f7077cb2493
Ordna-dev <ordnajelaorerreug@gmail.com>
2023-10-24 20:29:46 -0600

    voy a hacer la division de los datos

--- Load_dataset/functions.py
+++ Load_dataset/functions.py
@@ -1,6 +1,6 @@
 import pandas
 
-read = pandas.read_csv("Load_dataset/cerealPython.csv")
+read = pandas.read_csv("Load_dataset/cerealPython.csv") #ejemplo de uso de pandas
 
 def printCSV():
     return read.head()
