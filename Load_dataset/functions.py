f5ddd1c0c35899886b50018f3a5f9695e6d858ee
Ordna-dev <ordnajelaorerreug@gmail.com>
2023-10-25 20:39:25 -0600

    El usuario puede seleccionar cualquier archivo csv que desee

--- Load_dataset/functions.py
+++ Load_dataset/functions.py
@@ -1,6 +1,19 @@
 import pandas
+import tkinter
+from tkinter import filedialog
 
-read = pandas.read_csv("Load_dataset/cerealPython.csv") #ejemplo de uso de pandas
+#read = pandas.read_csv("Load_dataset/cerealPython.csv") #ejemplo de uso de pandas
 
 def printCSV():
+
+    window = tkinter.Tk()
+    window.withdraw()
+
+    filename = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
+
+    try:
+        read = pandas.read_csv(filename)
+    except FileNotFoundError:
+        print(f"No se encontro el archivo {filename}.")
+
     return read.head()
