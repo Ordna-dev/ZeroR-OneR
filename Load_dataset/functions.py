37d1c42a9c9586693a2c9ac8051597cf5cd1f5e1
Ordna-dev <ordnajelaorerreug@gmail.com>
2023-10-28 19:47:06 -0600

    Tabla de frecuencias en OneR hecha, estoy acabando el algoritmo

--- Load_dataset/functions.py
+++ Load_dataset/functions.py
@@ -26,14 +26,22 @@ def main(): #Funcion que se encarga de leer el archivo csv y ejecuta las funcion
     data = read.head(50) if filename else None #Leer maximo 50 instancias
 
     if data is not None:
-        train, test = divideData(data) #Se asignan las instancias a los conjuntos de entrenamiento y prueba
-        #class_attribute = random.choice(data.columns)
+        test, train = divideData(data) #Se asignan las instancias a los conjuntos de entrenamiento y prueba
+
         print("\nConjunto de entrenamiento:\n")
-        print(test)
-        #testing(test, class_attribute)
-        print("\nConjunto de prueba:\n")
         print(train)
-        #testing(train, class_attribute)
+
+        print("\nConjunto de prueba:\n")
+        print(test)
+        
+        class_attribute = input("\nIntroduce el nombre del atributo clase: ")
+
+        print("\nOneR - Alejandro Guerrero Moya")
+        print("\nTabla de frencuencias en el conjunto de entrenamiento:")
+        oneR(train, class_attribute)
+
+        print("\nTabla de frecuencias en el conjunto de prueba:")
+        oneR(test, class_attribute)
 
         #Aca van a llamar las funciones de zeroR y oneR
 
@@ -60,10 +68,40 @@ def testing(data, class_attribute): #Función puramente para pruebas, pueden tom
 
     print(f"\nRecuentos para el atributo clase {class_attribute}:\n{counts_string}")
 
-def zeroR(): #Parte de Esparza
+def zeroR(): #Parte de Isaac
 
     return 0
 
-def oneR(): #Parte de Isaac
+def oneR(data, class_attribute): #Algoritmo de OneR hecho por Alejandro Guerrero Moya
+    # Tabla de frecuencias
 
-    return 0
+    # Se obtienen todos los valores únicos del atributo clase
+    class_values = data[class_attribute].unique()
+
+    # Iteracion sobre cada columna
+    for column in data.columns:
+
+        #Si la columna es la clase atributo, se salta
+        if column == class_attribute:
+            continue
+        
+        # Se crea la tabla de frecuencias
+        frequency_table = data.groupby([column, class_attribute]).size()
+
+        # Se imprime el nombr del atributo
+        print(f"\nAtributo: {column}.")
+
+        # Iteracion sobre cada valor unico en la columna
+
+        for value in data[column].unique():
+            # Imprimir conteos para vada valor de la clase atributo
+            counts = []
+            for class_value in class_values:
+                count = frequency_table.get((value, class_value), 0)
+                counts.append(f"{class_value}: {count}")
+
+            print(f"Valor: {value}. {', '.join(counts)}.")
+    
+    #Tabla de reglas
+
+    #Resultado del algoritmo
